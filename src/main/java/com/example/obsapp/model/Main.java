c72995239f3da379f6ccefbef69b2cebfaeb4851
abdulsametpamuk <21181616015@gazi.edu.tr>
2025-11-30 16:23:18 +0300

    denenme2

--- src/main/java/com/example/obsapp/model/Main.java
+++ src/main/java/com/example/obsapp/model/Main.java
@@ -7,6 +7,7 @@ public class Main {
         String metin ;
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the metin name: ");
+        System.out.println("JAva proje");
 
     }
 }
