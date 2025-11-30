bfabbc3415eb6905a82a4f6eed4fcd48f39ed5f0
EmirhanArsla <24181616053@gazi.edu.tr>
2025-11-30 16:14:02 +0300

    deneme1

---
+++ src/main/java/com/example/obsapp/model/Main.java
@@ -0,0 +1,10 @@
+package com.example.obsapp.model;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String metin ;
+        Scanner sc = new Scanner(System.in);
+    }
+}
