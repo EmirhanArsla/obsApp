8005ea2a2885c6f73401a995b874f1e5c283a60f
EmirhanArsla <24181616053@gazi.edu.tr>
2025-11-30 16:17:19 +0300

    a

--- src/main/java/com/example/obsapp/model/Main.java
+++ src/main/java/com/example/obsapp/model/Main.java
@@ -6,5 +6,7 @@ public class Main {
     public static void main(String[] args) {
         String metin ;
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the metin name: ");
+
     }
 }
